bbf098460002a86c92101baad3b455d255d22351
heyjoshlee <lee45276@gmail.com>
2024-03-11 23:07:09 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+import * as c from './calculator.js';
+
+console.log(c.APIKEY)
---
+++ calculator.js
@@ -0,0 +1,18 @@
+export function add (a, b) {
+  return a + b;
+}
+
+export function subtract (a, b) {
+  return a - b;
+}
+
+export function multiply (a, b) {
+  return a * b;
+}
+
+export function divide (a, b) {
+  return a / b;
+}
+
+
+export const APIKEY = "123"
